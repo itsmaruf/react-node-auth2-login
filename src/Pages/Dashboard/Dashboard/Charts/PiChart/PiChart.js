eca406f08064ae1eaa87d4774da93a8209c6ece5
itsmaruf <maruf@corexlab.dev>
2022-12-15 17:25:47 +0600

    pi chart initiated

--- src/Pages/Dashboard/Dashboard/Charts/PiChart/PiChart.js
+++ src/Pages/Dashboard/Dashboard/Charts/PiChart/PiChart.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { PieChart, Pie, Sector, Cell, ResponsiveContainer } from "recharts";
+import { PieChart, Pie, Cell } from "recharts";
 import "./PiChart.css";
 
 const data = [
