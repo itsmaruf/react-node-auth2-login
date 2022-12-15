30cf059f07f1d9fd2e435de178ff64b86a2a1aa5
itsmaruf <maruf@corexlab.dev>
2022-12-15 13:33:58 +0600

    top part of the dashboard is done

--- src/Pages/Dashboard/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard/Dashboard.js
@@ -1,9 +1,32 @@
 import React from "react";
+import "./Dashboard.css";
+import { FaRegCalendarAlt } from "react-icons/fa";
+import { BsBoxArrowUp } from "react-icons/bs";
 
 const Dashboard = () => {
   return (
     <div className="p-6">
-      <div className="min-h-full bg-white p-4"></div>
+      <div className="min-h-full bg-white p-4">
+        <div className="flex justify-between items-center">
+          <div className="user">
+            <h2 className="text-xl font-semibold text-blue">
+              Hi, <span>Will Smith</span>
+            </h2>
+            <p className="text-gray-500 text-sm">Welcome to Loopmee</p>
+          </div>
+          <div className="date-info flex justify-end items-center">
+            <button className="btn btn-outline btn-sm mr-3">
+              <FaRegCalendarAlt className="mr-2 text-xl text-gray-500" />{" "}
+              <span className="text-xs text-gray-500">
+                Jul 05, 2021 - Jul 06, 2021
+              </span>
+            </button>
+            <button className="btn btn-blue btn-sm">
+              Export <BsBoxArrowUp />
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
