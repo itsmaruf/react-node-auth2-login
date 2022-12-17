39dd4929acafeca2374c76bfc5bfac1f78a9e524
itsmaruf <maruf@corexlab.dev>
2022-12-17 11:08:49 +0600

    pi chart completed

--- src/Pages/Dashboard/Dashboard/Charts/PiChart/PiChart.js
+++ src/Pages/Dashboard/Dashboard/Charts/PiChart/PiChart.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { PieChart, Pie, Cell } from "recharts";
+import { BiDotsHorizontalRounded } from "react-icons/bi";
 import "./PiChart.css";
 
 const data = [
@@ -15,8 +16,11 @@ const COLORS = ["#E371E7", "#571159", "#A8B8D8", "#FFFFFF", "#A8A2A5"];
 const PiChart = () => {
   return (
     <>
-      <h2 className="text-xl text-gray-600 font-bold">Reached Audience</h2>
-      <PieChart width={400} height={420} className="mx-auto">
+      <div className="flex justify-between items-center">
+        <h2 className="text-xl text-gray-600 font-bold">Reached Audience</h2>
+        <BiDotsHorizontalRounded className="text-xl" />
+      </div>
+      <PieChart width={400} height={295} className="mx-auto">
         <Pie
           data={data}
           cx="50%"
@@ -32,6 +36,39 @@ const PiChart = () => {
           ))}
         </Pie>
       </PieChart>
+
+      <div className="divider"></div>
+
+      <div className="flex lg:flex-row flex-col justify-between items-start">
+        <div className="item">
+          <div className="flex justify-start items-center">
+            <div className="ind men"></div>
+            Men
+          </div>
+          <h2 className="text-3xl font-bold">48%</h2>
+        </div>
+        <div className="item">
+          <div className="flex justify-start items-center">
+            <div className="ind women"></div>
+            Women
+          </div>
+          <h2 className="text-3xl font-bold">20%</h2>
+        </div>
+        <div className="item">
+          <div className="flex justify-start items-center">
+            <div className="ind children"></div>
+            Children
+          </div>
+          <h2 className="text-3xl font-bold">18%</h2>
+        </div>
+        <div className="item">
+          <div className="flex justify-start items-center">
+            <div className="ind other"></div>
+            Other
+          </div>
+          <h2 className="text-3xl font-bold">15%</h2>
+        </div>
+      </div>
     </>
   );
 };
